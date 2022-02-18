46f3972e8d1dd7f07b996a727f3701237c6d1ff0
seobkim <kks7824@gmail.com>
2022-02-18 20:14:07 +0900

    94p

--- projects/nwitter/src/index.js
+++ projects/nwitter/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
 
-//StringMode 설명 책 63Page 설명 보고 주석 달것!
 ReactDOM.render(
+  // CRA(Create React App)로 만든 리액프 프로젝트는 기본적으로 index.js 파일에 React.StrictMode를 설정함
+  // 이 설정을 지우지 않으면 console.log 함수를 2번 실행되는 현상이 나타남
+  // 개발상황에서 의도적으로 2번 호출하도록 만든 것
   <React.StrictMode>
     <App />
   </React.StrictMode>,
--- projects/nwitter/src/routes/Auth.js
+++ projects/nwitter/src/routes/Auth.js
@@ -1,3 +1,4 @@
+import { authService } from "fbase";
 import { useState } from "react";
 
 const Auth = () => {
@@ -5,7 +6,8 @@ const Auth = () => {
     //hooks useState 함수 공부공부공부//
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] =useState("");
     const onChange = (event) =>{
         const{
             target: {name, value},
@@ -18,17 +20,37 @@ const Auth = () => {
     };
     console.log(email+" : "+password);
 
-    const onSubmit = (event) =>{
-        event.preventDefault();
+    const onSubmit = async (event) =>{
+        try{
+            event.preventDefault();
+            let data;
+
+            if(newAccount){
+                //create new account
+                data = await authService.createUserWithEmailAndPassword(email,password);
+            }else{
+                //log in
+                data = await authService.signInWithEmailAndPassword(email,password);
+            }
+            console.log(data);
+        }catch(error){
+            setError(error.message);
+        }
     };
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
 
     return(
         <div>
             <form onSubmit={onSubmit}>
                 <input name= "email" type="email" placeholder="Email" required value={email} onChange={onChange}></input>
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}></input>
-                <input type='submit' value="Log In"></input>
+                <input type='submit' value={newAccount ? "Create Account" : "Log in"}></input>
+                {error}
             </form>
+            <span onClick = {toggleAccount}>
+                {newAccount ? "Sign in" : "Create Account"}
+            </span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>

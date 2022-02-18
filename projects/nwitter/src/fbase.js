aff4f7fea70213bd6fb6806e245599ab9175e1fc
seobkim <kks7824@gmail.com>
2022-02-18 23:58:17 +0900

    git error fixed

---
+++ projects/nwitter/src/fbase.js
@@ -0,0 +1,19 @@
+// firebase/app 내에 포함된 모든 모듈을 firebase란 객체에 부여
+// 책 40P firebase 임포트 시 "firebase/app" 으로만 불러오는데
+// firebase 버전 변경으로 "firebase/compat/app" 으로 불러와야 오류나지 않음
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
+export const authService = firebase.auth();
---
+++ src/components/Router.js
@@ -0,0 +1,25 @@
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+
+//위에서 Router 란 이름으로 import 를 정의하여서 상수 명칭 중복안되도록 AppRouter로 정의
+const AppRouter=({isLoggedIn})=>{
+    return(
+        <Router>
+            {/*Switch 사용하면 여러가지 Route중 하나만 렌더링 함*/}
+            <Switch>
+                {isLoggedIn?(
+                    <Route exact path ="/">
+                        <Home />
+                    </Route>
+                ):(
+                    <Route exact path = "/">
+                        <Auth />
+                    </Route>
+                )}
+            </Switch>
+        </Router>
+    )
+}
+
+export default AppRouter;

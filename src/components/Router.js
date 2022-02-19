62bc677a6d37484ebc9473a922c9dd1b8d6a59e5
seobkim <kks7824@gmail.com>
2022-02-19 19:09:09 +0900

    132p action_01

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,7 +5,7 @@ import Profile from "routes/Profile";
 import Navigation from "./Navigation";
 
 //위에서 Router 란 이름으로 import 를 정의하여서 상수 명칭 중복안되도록 AppRouter로 정의
-const AppRouter=({isLoggedIn})=>{
+const AppRouter=({isLoggedIn, userObj})=>{
     return(
         <Router>
             {/*Switch 사용하면 여러가지 Route중 하나만 렌더링 함*/}
@@ -14,7 +14,7 @@ const AppRouter=({isLoggedIn})=>{
                 {isLoggedIn?(
                 <>
                     <Route exact path ="/">
-                        <Home />
+                        <Home userObj = {userObj}/>
                     </Route>
                     <Route exact path = "/profile">
                         <Profile/>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,18 +1,49 @@
 import { dbService } from "fbase";
-import { useState } from "react";
-const Home = () =>{
+import { useEffect, useState } from "react";
+import Nweet from "components/Nweet";
+const Home = ({userObj}) =>{
 
     const[nweet, setNweet] = useState("");
-    
+    const[nweets, setNweets] = useState([]);    
+
+    //실시간 처리를 위한 예제
+    /* 
+    const getNweets = async () =>{
+        const dbNweets = await dbService.collection("nweets").get();
+        console.log(dbNweets);
+
+        dbNweets.forEach((document) => {
+        const newObject = {...document.data(), id : document.id};
+            setNweets((prev) => [newObject, ...prev])
+        }
+        );
+    }
+    */
+
+    useEffect(() => {
+        dbService.collection("nweets").onSnapshot((snapshot) =>{
+            const newArray = snapshot.docs.map((document) => ({
+                id : document.id,
+                ...document.data(),
+            }));
+            setNweets(newArray);
+        });
+        // 실시간 처리를 위한 예제 함수 호출
+        // getNweets();
+    },[]);
+
+    // Nweet(글 작성) 버튼 클릭 시 Event
     const onSubmit = async (event) =>{
         event.preventDefault();
         await dbService.collection("nweets").add({
-            text:nweet,
-            createAt:Date.now(),
+            text : nweet,
+            createAt : Date.now(),
+            creatorId : userObj.uid,
         });
         setNweet("");
     };
-
+    
+    // 글 작성 칸 변경되었을시 Event
     const onChange = (event) =>{
         event.preventDefault();
         const{
@@ -22,10 +53,17 @@ const Home = () =>{
     };
 
     return(
-        <form onSubmit={onSubmit}>
-            <input value = {nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}></input>
-            <input type = "submit" value="Nweet"></input>
-        </form>
+        <>
+            <form onSubmit={onSubmit}>
+                <input value = {nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}></input>
+                <input type = "submit" value="Nweet"></input>
+            </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <Nweet key={nweet.id} nweetObj ={nweet}></Nweet>
+                ))}
+            </div>
+        </>
     )
 }
 
